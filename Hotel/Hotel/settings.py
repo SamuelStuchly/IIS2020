f39bc8e166a44fb4b46ba018b0a03d3ab5aaccde
SamuelStuchly <sssstuchly@gmail.com>
2020-12-01 22:59:49 +0100

    settings.py updated

--- Hotel/Hotel/settings.py
+++ Hotel/Hotel/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -155,7 +155,7 @@ LOGIN_URL = 'login'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
