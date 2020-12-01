763fcb95862047047c0da4df6e19caf28d018e12
SamuelStuchly <sssstuchly@gmail.com>
2020-12-01 07:39:36 +0100

    Procfile fix

--- Hotel/Hotel/settings.py
+++ Hotel/Hotel/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +141,5 @@ LOGIN_REDIRECT_URL = 'HotelApp-home'
 LOGIN_URL = 'login'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+django_heroku.settings(locals())
